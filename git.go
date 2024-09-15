140d705f6c268a88a4469f5ee951e1946e5b9d6f
gary zhu <garyzhu@garys-Air.fritz.box>
2024-09-15 14:17:18 +1200

    feat: ABC-013: adds getCommitMessages

--- git.go
+++ git.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"os/exec"
 	"regexp"
+	"strings"
 )
 
 func extractJiraTicketNumber(commitMessage string) ([]string, error) {
@@ -11,4 +15,23 @@ func extractJiraTicketNumber(commitMessage string) ([]string, error) {
 	}
 
 	return re.FindAllString(commitMessage, -1), nil
+}
+
+func getCommitMessages(startCommit, endCommit string) ([]string, error) {
+	var revisionRange = endCommit
+	if startCommit != "" {
+		revisionRange = fmt.Sprintf("%s..%s", startCommit, endCommit)
+	}
+	cmd := exec.Command("git", "log", "--oneline", revisionRange)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+
+	err := cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("error running git log command: %s", out.String())
+	}
+
+	commitMessages := strings.Split(strings.TrimSpace(out.String()), "\n")
+	return commitMessages, nil
 }
